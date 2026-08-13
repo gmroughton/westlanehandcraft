0debbc70cf2e2d2cea32d1428bce75a67a2abb72
Greg Roughton <gmroughton@hey.com>
2026-08-12 20:06:33 -0400

    Laravel 12 and Lunar installation

---
+++ app/Models/User.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Lunar\Base\Traits\LunarUser;
+use Lunar\Base\LunarUser as LunarUserInterface;
+
+#[Fillable(['name', 'email', 'password'])]
+#[Hidden(['password', 'remember_token'])]
+class User extends Authenticatable implements LunarUserInterface
+{
+    /** @use HasFactory<UserFactory> */
+    use HasFactory, Notifiable, LunarUser;
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+}
